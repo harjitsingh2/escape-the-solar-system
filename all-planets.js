93139f5b67501e37784f5db1641512e80aa13611
harjitsingh3 <singh.harjit09@gmail.com>
2023-05-09 22:37:54 -0400

    Create all-planets.js and add facts and questions

---
+++ all-planets.js
@@ -0,0 +1,290 @@
+let mercury = [
+    {
+        question: "Mercury might be the smallest planet in our solar system, but it is also the fastest - traveling 29 miles per second! This is due to its proximity to the Sun. Even though it is so close to the Sun, it is not the hottest planet in our solar system. Venus is actually the hottest. Mercury has no rings or moons. The small, terrestrial planet does not have an atmosphere. It does have a thin exosphere made of oxygen, sodium, hydrogen, helium, and potassium, in descending order."
+    },
+    {
+        question: "Mercury is the _______ planet from the Sun",
+        answers: {
+            a: "first",
+            b: "second",
+            c: "third"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "What is Mercury's exosphere primarily composed of?",
+        answers: {
+            a: "hydrogen",
+            b: "mercury",
+            c: "oxygen"
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: "How fast does Mercury travel?",
+        answers: {
+            a: "29 miles per second",
+            b: "290 miles per second",
+            c: "2900 miles per second"
+        },
+        correctAnswer: 'a'
+    }
+];
+
+let venus = [
+    {
+      question: "Venus, aka 'Earth's twin', has a size and structure similar to Earth. But it has a dense and toxic atmosphere filled with carbon dioxide and sulfuric acid which has led to extreme surface heat. Venus rotates very slowly. One day on Venus is 243 Earth days and one year is 225 Earth days. Meaning that a Venusian day is longer than a year! Additionally, Venus rotates in the opposite direction of most planets. It is quite the oddball."
+    },  
+    {
+          question: "Venus is the hottest planet in our solar system",
+          answers: {
+              a: "true",
+              b: "false"
+          },
+          correctAnswer: 'a'
+      },
+      {
+          question: "Approximately how long is one day on Venus?",
+          answers: {
+              a: "1 Earth day",
+              b: "100 Earth days",
+              c: "200 Earth days"
+          },
+          correctAnswer: 'c'
+      },
+      {
+          question: "Which of these can be found in Venus' atmosphere?",
+          answers: {
+              a: "oxygen",
+              b: "ozone",
+              c: "sulfuric acid"
+          },
+          correctAnswer: 'c'
+      }
+  ];
+
+let earth = [
+    {
+      question: "Earth, the planet that we all know and love, is the third planet from the Sun. Although Earth is a rocky planet filled with mountains and canyons, most of its surface is covered with water. Earth's atmosphere is 78% nitrogen and 21% oxygen. Not only does Earth's atmosphere give us the ability to breathe, but it also protects us from meteoroids."
+    },  
+    {
+          question: "How long does it take Earth to rotate around its axis?",
+          answers: {
+              a: "24 hours",
+              b: "365 days"
+          },
+          correctAnswer: 'a'
+      },
+      {
+          question: "What covers a majority of Earth's surface?",
+          answers: {
+              a: "land",
+              b: "plants",
+              c: "water"
+          },
+          correctAnswer: 'c'
+      },
+      {
+          question: "What is Earth's atmosphere primarily composed of?",
+          answers: {
+              a: "nitrogen",
+              b: "oxygen",
+              c: "ozone"
+          },
+          correctAnswer: 'b'
+      }
+  ];
+
+let mars = [
+    {
+      question: "Mars is the only other planet that humans have explored with rovers, essentially making it our second home. Although Mars is mostly a cold and dusty desert, it does have incredible features like polar ice caps, extinct volcanoes, and canyons. Mars has no rings, but it does have two moons named Phobos and Deimos. There is no life currently on Mars. However, this doesn't rule out the possibility of there having been life on Mars."
+    },  
+    {
+          question: "Is there water on Mars?",
+          answers: {
+              a: "no",
+              b: "yes"
+          },
+          correctAnswer: 'b'
+      },
+      {
+          question: "How many moons does Mars have?",
+          answers: {
+              a: "0",
+              b: "1",
+              c: "2"
+          },
+          correctAnswer: 'c'
+      },
+      {
+          question: "Are there any living species on Mars?",
+          answers: {
+              a: "no",
+              b: "yes"
+          },
+          correctAnswer: 'a'
+      }
+  ];
+
+let jupiter = [
+    {
+      question: "The 'gas giant' Jupiter is the largest planet in our solar system. It could fit eleven Earths across its diameter. Jupiter's atmosphere is made primarily of hydrogen and helium. Due to its large mass, Jupiter has a strong gravitational pull. More than 75 moons orbit this giant. One day on Jupiter is equal to 10 hours, while a year is 12 Earth years."
+    },  
+    {
+          question: "How many hours is one day on Jupiter?",
+          answers: {
+              a: "10 hours",
+              b: "24 hours",
+              c: "50 hours"
+          },
+          correctAnswer: 'a'
+      },
+      {
+          question: "What is Jupiter's atmosphere primarily composed of?",
+          answers: {
+              a: "hydrogen",
+              b: "nitrogen",
+              c: "oxygen"
+          },
+          correctAnswer: 'a'
+      },
+      {
+          question: "How many moons does Jupter have?",
+          answers: {
+              a: "0",
+              b: "5",
+              c: "75"
+          },
+          correctAnswer: 'c'
+      }
+  ];
+
+let saturn = [
+    {
+      question: "Saturn might not be the only planet that has rings, but its rings are the most prominent and beautiful rings in our solar system. Similar to Jupiter, Saturn is also a gas planet made mostly of hydrogen and helium. Saturn cannot sustain life, but some of its moons, notably Enceladus, have the potential to sustain life."
+    },  
+    {
+          question: "Saturn is the only planet with rings.",
+          answers: {
+              a: "false",
+              b: "true"
+          },
+          correctAnswer: 'a'
+      },
+      {
+          question: "What is Saturn's atmosphere primarily composed of?",
+          answers: {
+              a: "hydrogen",
+              b: "nitrogen",
+              c: "oxygen"
+          },
+          correctAnswer: 'a'
+      },
+      {
+          question: "Saturn is the _______ planet from the Sun.",
+          answers: {
+              a: "fifth",
+              b: "sixth",
+              c: "seventh"
+          },
+          correctAnswer: 'b'
+      }
+  ];
+
+let uranus = [
+    {
+      question: "Uranus was discovered in 1781 by William Herschel, making it the first planet to be discovered with a telescope. Uranus is composed primarily of icy water, methane, and ammonia, making it an 'ice giant'. It also shares unique properties with other planets: like Saturn, it has 13 rings, and like Venus, it rotates east to west."
+    },  
+    {
+          question: "There is water on Uranus",
+          answers: {
+              a: "false",
+              b: "true"
+          },
+          correctAnswer: 'b'
+      },
+      {
+          question: "How many rings does Uranus have?",
+          answers: {
+              a: "5",
+              b: "7",
+              c: "13"
+          },
+          correctAnswer: 'c'
+      },
+      {
+          question: "On Uranus, the Sun rises in the _______ and sets in the ______.",
+          answers: {
+              a: "east, west",
+              b: "west, east",
+              c: "north, south"
+          },
+          correctAnswer: 'b'
+      }
+  ];
+
+let neptune = [
+    {
+      question: "The eight and final planet in our solar system is Neptune. It is 30 times further from the Sun that Earth. Neptune is not visible by the naked eye when we look up to the sky on Earth. Neptune's 14 moons are named after sea gods and nymphs in Greek mythology. Neptune is so far from the Sun that 1 Neptunian year is equal to 165 Earth years."
+    },  
+    {
+          question: "Neptune is the last planet in our solar system",
+          answers: {
+              a: "false",
+              b: "true"
+          },
+          correctAnswer: 'b'
+      },
+      {
+          question: "How long does it take for Neptune to orbit the Sun?",
+          answers: {
+              a: "20 Earth years",
+              b: "105 Earth years",
+              c: "165 Earth years"
+          },
+          correctAnswer: 'c'
+      },
+      {
+          question: "Neptune is an ice giant, with similar composition to Uranus. Which of these elements do not exist on Neptune?",
+          answers: {
+              a: "methane",
+              b: "neptunium",
+              c: "water"
+          },
+          correctAnswer: 'b'
+      }
+  ];
+
+let pluto = [
+    {
+      question: "Pluto is by far the most controversial celestial body in our solar system. Once a planet, Pluto has been 'demoted' to dwarf planet status. The main reason for this is that Pluto's orbit, size, and composition is more similar to other objects in the Kuiper Belt. So it was demoted, it simply changed teams. Interestingly, Pluto has its own team within its orbit. Pluto has 5 moons. The biggest moon, Charon, is almost the same size as Pluto."
+    },  
+    {
+          question: "Is Pluto a planet?",
+          answers: {
+              a: "no",
+              b: "yes",
+              c: "YES",
+              d: "YESSS!!!"
+          },
+          correctAnswer: 'a'
+      },
+      {
+          question: "Where in the solar system is Pluto located?",
+          answers: {
+              a: "Asteroid belt",
+              b: "Comet belt",
+              c: "Kuiper belt"
+          },
+          correctAnswer: 'c'
+      },
+      {
+          question: "But seriously, is Pluto a planet?",
+          answers: {
+              a: "no",
+              b: "yes of course it is",
+              c: "technically...it's a dwarf planet"
+          },
+          correctAnswer: 'c'
+      }
+  ];
