edbbb1dcc5392b7126ba3ead5a2edd6b66b79ca3
harjitsingh3 <singh.harjit09@gmail.com>
2023-05-14 13:23:56 -0400

    Create interplanetary transitions

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -9,12 +9,36 @@ function loadPlanet(currentPlanetIndex) {
 }
 
 const planetButton = document.getElementById('next-planet');
+const body = document.querySelector('.planet');
+const transition = document.querySelector('#planet-transition');
 
 planetButton.addEventListener('click', () => {
     currentPlanetIndex++;
-    if (currentPlanetIndex < planetFiles.length) {
-        loadPlanet(currentPlanetIndex);
-    }
+    transition.style.display = 'block';
+    body.classList.add('fade-out');
+
+    setTimeout(() => {
+        body.classList.remove('fade-out');
+        body.classList.add('fade-in');
+        // transition.style.display = 'block';
+        // transition.classList.add('fade-in')
+        
+
+        setTimeout(() => {
+            body.classList.remove('fade-in');
+            // transition.classList.remove('fade-in')
+            transition.style.display = 'none';
+        }, 500)
+
+        if (currentPlanetIndex < planetFiles.length) {
+            loadPlanet(currentPlanetIndex);
+        }
+
+    }, 2000)
+
+    // if (currentPlanetIndex < planetFiles.length) {
+    //     loadPlanet(currentPlanetIndex);
+    // }
 })
 
 loadPlanet(currentPlanetIndex);
